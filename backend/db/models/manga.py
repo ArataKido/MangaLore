e991fbe1f13e56e61c74239884f2330b75ee277f
Arata <shahzod030905@gmail.com>
2023-07-22 16:30:26 +0200

    seted up database

--- backend/db/models/manga.py
+++ backend/db/models/manga.py
@@ -1,2 +1,61 @@
-from sqlalchemy import ForeignKey, String, Integer, BigInteger
-from sqlalchemy.orm import relationship
+from backend.db.models.base import Base
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship, backref
+
+
+class Manga(Base):
+    __tablename__   = "manga"
+
+    id                         = Column(Integer, primary_key=True, index=True)
+    tag_id                     = Column(Integer, ForeignKey('tag.id'), primary_key=True)
+    genre_id                   = Column(Integer, ForeignKey('genre.id'), primary_key=True)
+    bookmark_id                = Column(Integer, ForeignKey('bookmark.id'), primary_key=True)
+    user_manga_status_id       = Column(Integer, ForeignKey('usermangastatus.id'), primary_key=True)
+
+    title                      = Column(String,)
+    alt_title                  = Column(String)
+    manga_status               = Column(String,)
+    images                     = Column(String,)
+    
+    genre                      = relationship('Genre', back_populates='manga', )
+    tag                        = relationship('Tag', back_populates='manga', )
+    bookmark                   = relationship('Bookmark', back_populates='manga', cascade='all, delete-origin')
+    user_manga_status          = relationship('UserMangaStatus', back_populates='manga', cascade='all, delete-origin')
+
+class Tag(Base):
+    __tablename__               = "tag"
+
+    id                          = Column(Integer, primary_key=True, index=True)
+    name                        = Column(String, unique=True, index=True)
+
+    manga                       = relationship('Manga', secondary='tag',)
+
+class Genre(Base):
+    __tablename__               = "genre"
+
+    id                          = Column(Integer, primary_key=True, index=True)
+    name                        = Column(String, unique=True, index=True)
+
+    manga                       = relationship('Manga', secondary='genre',)
+
+
+class Bookmark(Base):
+    __tablename__               = 'bookmark'
+    
+    id                          = Column(Integer, primary_key=True, index=True)
+    manga_id                    = Column(Integer, ForeignKey('manga.id'), primary_key=True)
+    user_id                     = Column(Integer, ForeignKey('user.id'), primary_key=True)
+
+    manga                       = relationship('Manga', secondary='bookmark',)
+    user                        = relationship('User', secondary='bookmark')
+
+class UserMangaStatus(Base):
+    __tablename__               = 'user_manga_status'
+
+    id                          = Column(Integer, primary_key=True, index=True)
+    manga_id                    = Column(Integer, ForeignKey('manga.id'), primary_key=True)
+    user_id                     = Column(Integer, ForeignKey('user.id'), primary_key=True)
+    status                      = Column(String, default=None)
+
+    manga                       = relationship('Manga', secondary='user_manga_status',)
+    user                        = relationship('User', secondary='user_manga_status')
--- backend/db/models/user.py
+++ backend/db/models/user.py
@@ -1,39 +1,18 @@
 from backend.db.models.base import Base
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, backref
 
 class User(Base):
-    __tablename__ = "users"
+    __tablename__   = "user"
 
-    id          = Column(Integer, primary_key=True, index=True)
-    email       = Column(String, unique=True, index=True)
-    password    = Column(String,)
-    is_acitve   = Column(Boolean, default=True)
+    id                      = Column(Integer, primary_key=True, index=True)
+    bookmark_id             = Column(Integer, ForeignKey('bookmark.id'), primary_key=True)
+    user_manga_status_id    = Column(Integer, ForeignKey('usermangastatus.id'), primary_key=True)
 
+    email                   = Column(String, unique=True, index=True)
+    password                = Column(String,)
+    nickname                = Column(String)
+    is_acitve               = Column(Boolean, default=True)
 
-class Manga(Base):
-    __tablename__ = "manga"
-
-    id          = Column(Integer, primary_key=True, index=True)
-    title       = Column(String,)
-    alt_title   = Column(String)
-    status      = Column(String,)
-
-class Tag(Base):
-    __tablename__ = "tags"
-
-    id          = Column(Integer, primary_key=True, index=True)
-    name        = Column(String, unique=True, index=True)
-
-class Genre(Base):
-    __tablename__ = "genres"
-
-    id          = Column(Integer, primary_key=True, index=True)
-    name        = Column(String, unique=True, index=True)
-
-
-class MangaTagGenre(Base):
-    __tablename__ = 'manga_tag_genre'
-    manga_id    = Column(Integer, ForeignKey(Manga.id), primary_key=True) 
-    tag_id      = Column(Integer, ForeignKey(Tag.id), primary_key=True)
-    genre_id    = Column(Integer, ForeignKey(Genre.id), primary_key=True)
+    bookmark                = relationship('Bookmark', back_populates='user', cascade='all, delete-orphan')
+    user_manga_status       = relationship('UserMangaStatus', back_populates='user', cascade='all, delete-orphan')
